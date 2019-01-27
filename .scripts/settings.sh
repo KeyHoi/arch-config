e762b662fe96f8a78cd9e7272095a816f859ebdf
Keyhoi <phamdominik1805@gmail.com>
2019-01-27 13:02:19 +0100

    added youtube-dl script

--- .scripts/settings.sh
+++ .scripts/settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choice="$(printf sound'\n'monitor'\n'connections'\n'appearance | dmenu -i -p "What do you want to do?" -fn monospace -sb '#42f45f' -sf '#000000' -nb '#000000' -nf '#42f45f')"
+choice="$(printf sound'\n'monitor'\n'connections'\n'appearance | dmenu -i -p "settings" -fn 'monospace:size=12' -sb '#42f45f' -sf '#000000' -nb '#000000' -nf '#42f45f')"
 
 case "$choice" in 
 	"sound")
